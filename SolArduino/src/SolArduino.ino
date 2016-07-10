b3c8468a346baac11b32ca78f0777a1c0299baea
Thomas Schouten <t.m.schouten@student.tue.nl>
2016-07-10 21:51:48 +0200

    adapted code to display working webpage

--- SolArduino/src/SolArduino.ino
+++ SolArduino/src/SolArduino.ino
@@ -10,9 +10,11 @@
 
 static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
 
-static byte myip[] = {192, 168, 2, 14};
+static byte myip[] = {192, 168, 2, 10};
 
-byte Ethernet::buffer[700];
+byte Ethernet::buffer[500];
+
+BufferFiller bfill;   //used in homepage
 
 void setup () {
   Serial.begin(9600);
@@ -29,19 +31,42 @@ void setup () {
   if (ether.begin(sizeof Ethernet::buffer, mymac, 10) == 0)
     Serial.println(F("Failed to access Ethernet controller"));
 
-    // ether.staticSetup(myip);
+    ether.staticSetup(myip);
     //
     // ether.printIp("My IP: ", ether.myip);
     // ether.printIp("GW IP: ", ether.gwip);
 
-  Serial.println(F("Setting up DHCP"));
-  if (!ether.dhcpSetup())
-    Serial.println(F("DHCP failed"));
+  // Serial.println(F("Setting up DHCP"));
+  // if (!ether.dhcpSetup())
+  //   Serial.println(F("DHCP failed"));
 
   ether.printIp("My IP: ", ether.myip);
-  ether.printIp("Netmask: ", ether.netmask);
-  ether.printIp("GW IP: ", ether.gwip);
-  ether.printIp("DNS IP: ", ether.dnsip);
+  // ether.printIp("Netmask: ", ether.netmask);
+  // ether.printIp("GW IP: ", ether.gwip);
+  // ether.printIp("DNS IP: ", ether.dnsip);
 }
 
-void loop () {}
+void loop () {
+ word len = ether.packetReceive();
+ word pos = ether.packetLoop(len);
+ if (pos)  // check if valid tcp data is received
+   ether.httpServerReply(homePage()); // send web page data
+}
+
+static word homePage() {
+ long t = millis() / 1000;
+ word h = t / 3600;
+ byte m = (t / 60) % 60;
+ byte s = t % 60;
+ bfill = ether.tcpOffset();                         //???????
+ bfill.emit_p(PSTR(                                 //???????
+   "HTTP/1.0 200 OK\r\n"
+   "Content-Type: text/html\r\n"
+   "Pragma: no-cache\r\n"
+   "\r\n"
+   "<meta http-equiv='refresh' content='1'/>"
+   "<title>RBBB server</title>"
+   "<h1>$D$D:$D$D:$D$D</h1>"),
+     h/10, h%10, m/10, m%10, s/10, s%10);                //???????
+ return bfill.position();                                           //???????
+}
