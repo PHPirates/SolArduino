d0b075b76267384397b61becabda2dc0e7b7c6ab
Thomas Schouten <t.m.schouten@student.tue.nl>
2016-07-12 16:55:36 +0200

    implemented code to receive http request

--- SolArduino/src/SolArduino.ino
+++ SolArduino/src/SolArduino.ino
@@ -1,10 +1,5 @@
-// Arduino demo sketch for testing the DHCP client code
-//
-// Original author: Andrew Lindsay
-// Major rewrite and API overhaul by jcw, 2011-06-07
-//
-// Copyright: GPL V2
-// See http://www.gnu.org/licenses/gpl.html
+//code adapted from //http://forum.arduino.cc/index.php?topic=90269.msg1991068#msg1991068
+
 
 #include <EtherCard.h>
 
@@ -18,55 +13,63 @@ BufferFiller bfill;   //used in homepage
 
 void setup () {
   Serial.begin(9600);
-  Serial.println(F("\n[testDHCP]"));
-
-  Serial.print("MAC: ");
-  for (byte i = 0; i < 6; ++i) {
-    Serial.print(mymac[i], HEX);
-    if (i < 5)
-      Serial.print(':');
-  }
-  Serial.println();
 
   if (ether.begin(sizeof Ethernet::buffer, mymac, 10) == 0)
     Serial.println(F("Failed to access Ethernet controller"));
 
     ether.staticSetup(myip);
-    //
-    // ether.printIp("My IP: ", ether.myip);
-    // ether.printIp("GW IP: ", ether.gwip);
-
-  // Serial.println(F("Setting up DHCP"));
-  // if (!ether.dhcpSetup())
-  //   Serial.println(F("DHCP failed"));
-
-  ether.printIp("My IP: ", ether.myip);
-  // ether.printIp("Netmask: ", ether.netmask);
-  // ether.printIp("GW IP: ", ether.gwip);
-  // ether.printIp("DNS IP: ", ether.dnsip);
+    ether.printIp("Address: http://", ether.myip);
 }
 
 void loop () {
  word len = ether.packetReceive();
  word pos = ether.packetLoop(len);
- if (pos)  // check if valid tcp data is received
-   ether.httpServerReply(homePage()); // send web page data
+ if (pos) { // check if valid tcp data is received
+   delay(1); //just to be sure
+   bfill = ether.tcpOffset();
+   char *data = (char *) Ethernet::buffer + pos;
+   if (strncmp("GET /", data, 5) != 0) {
+          Serial.println("no valid GET request");
+     }
+     else {
+         data += 5;
+         if (data[0] == ' ') {
+             // Return home page
+             homePage();
+         }
+         else if (strncmp("?panel=up ", data, 10) == 0) {
+             Serial.println("up");
+         }
+         else if (strncmp("?panel=down ", data, 12) == 0) {
+             Serial.println("down");
+         }
+         else if (strncmp("?panel=stop ", data, 12) == 0) {
+             Serial.println("stop");
+         }
+         else if (strncmp("?panel=auto ", data, 12) == 0) {
+             Serial.println("auto");
+         }
+         else {
+             Serial.println("page not found");
+         }
+     }
+   ether.httpServerReply(bfill.position()); //send the reply, if there was one
+ }
 }
 
-static word homePage() {
+void homePage() {
  long t = millis() / 1000;
  word h = t / 3600;
  byte m = (t / 60) % 60;
  byte s = t % 60;
- bfill = ether.tcpOffset();                         //???????
- bfill.emit_p(PSTR(                                 //???????
+ bfill = ether.tcpOffset();
+ bfill.emit_p(PSTR(
    "HTTP/1.0 200 OK\r\n"
    "Content-Type: text/html\r\n"
    "Pragma: no-cache\r\n"
    "\r\n"
-   "<meta http-equiv='refresh' content='1'/>"
+  //  "<meta http-equiv='refresh' content='1'/>"
    "<title>RBBB server</title>"
    "<h1>$D$D:$D$D:$D$D</h1>"),
-     h/10, h%10, m/10, m%10, s/10, s%10);                //???????
- return bfill.position();                                           //???????
-}
+     h/10, h%10, m/10, m%10, s/10, s%10);
+  }
