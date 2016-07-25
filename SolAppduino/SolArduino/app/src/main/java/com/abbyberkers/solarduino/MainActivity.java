56b3798c4bfd7aafe697e55eb0ccb8b24fa258e0
slideclimb <abbyberkers@gmail.com>
2016-07-25 07:27:57 +0200

    fixed #15 and added fancy arrow buttons

--- SolAppduino/SolArduino/app/src/main/java/com/abbyberkers/solarduino/MainActivity.java
+++ SolAppduino/SolArduino/app/src/main/java/com/abbyberkers/solarduino/MainActivity.java
@@ -1,5 +1,7 @@
 package com.abbyberkers.solarduino;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.net.http.HttpResponseCache;
 import android.os.AsyncTask;
 import android.os.SystemClock;
@@ -14,12 +16,14 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
@@ -31,7 +35,13 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     String urlString;
-    String ipString = "http://192.168.0.23/";
+    String ipString = "http://192.168.2.10/"; // IP Thomas
+//    String ipString = "http://192.168.0.23/"; // IP Abby
+
+    int delay = 900;        // delay for the Timer/TimerTask
+
+    Timer downTimer;
+    Timer upTimer;
 
     TextView textView;
     TextView currentAngle;  // show current angle of solar panels
@@ -41,8 +51,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
     SeekBar seekbar;        // seekbar to change angle of solar panels
 
-    Button upButton;        // button to make the solar panels move up
-    Button downButton;      // button to make the solar panels move down
+    ImageButton upButton;   // button to make the solar panels move up
+    ImageButton downButton;      // button to make the solar panels move down
     Button setAngle;        // button to set angle of solar panels
 
     FrameLayout frameLayout;
@@ -82,8 +92,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                     }
                 });
 
-        upButton = (Button) findViewById(R.id.upButton);
-        downButton = (Button) findViewById(R.id.downButton);
+        upButton = (ImageButton) findViewById(R.id.upButton);
+        downButton = (ImageButton) findViewById(R.id.downButton);
         setAngle = (Button) findViewById(R.id.setAngle);
 
         frameLayout = (FrameLayout) findViewById(R.id.frame);
@@ -114,17 +124,30 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
             }
         });
 
-        // set OnTouchListener for the up button, send http request when putton pressed and released
+        // set OnTouchListener for the up button, send http request when button pressed and released
         upButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 switch (motionEvent.getAction()){
                     case MotionEvent.ACTION_DOWN:
                         textView.setText("Up button pressed.");
+                        upButton.setPressed(true);
                         sendDirectionRequest("up");
+
+                        upTimer = new Timer();
+                        TimerTask timerTask = new TimerTask() {
+                            @Override
+                            public void run() {
+                                sendUpdateRequest();
+                            }
+                        };
+
+                        upTimer.schedule(timerTask, 1500, delay);
                         break;
                     case MotionEvent.ACTION_UP:
                         textView.setText("Up button released.");
+                        upButton.setPressed(false);
+                        upTimer.cancel();
                         sendDirectionRequest("stop");
                         break;
                 }
@@ -132,17 +155,28 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
             }
         });
 
-        // set OnTouchListener for the down button, send http request when putton pressed and released
+        // set OnTouchListener for the down button, send http request when button pressed and released
         downButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 switch (motionEvent.getAction()){
                     case MotionEvent.ACTION_DOWN:
                         textView.setText("Down button pressed.");
+                        downButton.setPressed(true);
                         sendDirectionRequest("down");
+                        downTimer = new Timer();
+                        TimerTask timerTask = new TimerTask() {
+                            @Override
+                            public void run() {
+                                sendUpdateRequest();
+                            }
+                        };
+                        downTimer.schedule(timerTask, 1500, delay);
                         break;
                     case MotionEvent.ACTION_UP:
                         textView.setText("Down button released.");
+                        downButton.setPressed(false);
+                        downTimer.cancel();
                         sendDirectionRequest("stop");
                         break;
                 }
@@ -155,22 +189,24 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 switch (motionEvent.getAction()){
                     case MotionEvent.ACTION_DOWN:
+
                         Log.e("setAngle", "pressed");
                         int prog = seekbar.getProgress();
                         sendAngleRequest(prog);
                         break;
+
                     case MotionEvent.ACTION_UP:
+
                         Log.e("setAngle", "released");
-//                        String angleString = (currentAngle.getText().toString()).substring(0,2);
-//                        int angleInt = Integer.valueOf(angleString);
-//                        int seekBarProgress = seekbar.getProgress();
-//                        Log.e("angles", "current " + angleInt + " seekbar " + seekBarProgress);
-//
-//                        sendUpdateRequest();
 
+                        // a Timer and TimerTask to repeat the update request with interval 'delay',
+                        // until the solar panels reached the angle that is on the seekbar at that moment
                         final Timer timer = new Timer();
-
                         final TimerTask task = new TimerTask() {
+
+                            // String containing the current angle, taken from
+                            // (TextView) currentAngle, minus the degree symbol, trimmed to remove
+                            // possible spaces ("9 \u00b0" -> "9")
                             String angleString = (currentAngle.getText().toString())
                                     .substring(0,2)
                                     .trim();
@@ -181,7 +217,10 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                             public void run() {
                                 Log.e("angles", "current " + angleInt + " seekbar " + seekBarProgress);
 
-                                if(Math.abs(angleInt - seekBarProgress) == 1) {
+                                if(angleInt == seekBarProgress) {
+//                                if(Math.abs(angleInt - seekBarProgress) == 1) {
+
+                                    // cancel the timer when right angle is reached
                                     Log.w("timer", "cancel");
                                     timer.cancel();
                                 } else {
@@ -199,8 +238,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                             }
                         };
 
-                        timer.schedule(task, 800, 800);
-
+                        timer.schedule(task, delay, delay);
                         break;
                 }
                 return true;
@@ -240,6 +278,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
      * @param angle
      */
     public void sendAngleRequest(int angle){
+        Toast.makeText(getBaseContext(), "Angle set at " + angle + "\u00b0", Toast.LENGTH_SHORT).show();
 //        String urlString;
         if(angle < 10){
             urlString = ipString + "?degrees=0" + String.valueOf(angle);
@@ -247,6 +286,12 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
             urlString = ipString + "?degrees=" + String.valueOf(angle);
         }
 //        urlString = "http://pannenkoekenwagen.nl/pkw/test.html";
+        new SendRequest().execute(urlString);
+    }
+
+    public void sendUpdateRequest(View view) {
+        Toast.makeText(getBaseContext(), "Updating...", Toast.LENGTH_SHORT).show();
+        urlString = ipString + "?update";
         new SendRequest().execute(urlString);
     }
 
@@ -260,6 +305,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
      * class to do html stuff...
      */
     private class SendRequest extends AsyncTask<String, Void, String> {
+
         @Override
         protected String doInBackground(String... url){
             try{
@@ -271,19 +317,28 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         @Override
         protected void onPostExecute(String result) {
-            String message = urlString.substring(20);
-            Log.e("urlString", urlString);
-            Log.e("message", message);
-            if(message.contains("panel")){
-                responseTV.setText(Html.fromHtml(result));
-                Toast.makeText(getBaseContext(), Html.fromHtml(result), Toast.LENGTH_SHORT).show();
-            } else if(message.contains("degrees")) {
-                Log.e("result", result);
-            } else if(message.contains("update")) {
-                Log.e("result", result);
-                currentAngle.setText(Html.fromHtml(result));
-                result = result.substring(0,2).trim();
-                rotate(Integer.valueOf(result));
+            if(result.contains("Unable")){
+                Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
+                seekbar.setProgress(Integer.valueOf((currentAngle.getText().toString())
+                        .substring(0, 2)
+                        .trim()));
+                Log.w("internet", "not connected");
+            } else {
+                String message = urlString.substring(20);
+                Log.e("urlString", urlString);
+                Log.e("message", message);
+                if (message.contains("panel")) {
+                    responseTV.setText(Html.fromHtml(result));
+                    Toast.makeText(getBaseContext(), Html.fromHtml(result), Toast.LENGTH_SHORT).show();
+                } else if (message.contains("degrees")) {
+                    Log.e("result", result);
+                } else if (message.contains("update")) {
+                    Log.e("result", result);
+                    String angle = Html.fromHtml(result) + "\u00b0";
+                    currentAngle.setText(angle);
+                    result = result.substring(0, 2).trim();
+                    rotate(Integer.valueOf(result));
+                }
             }
         }
         private String sendRequest(String myURL) {
@@ -309,6 +364,9 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 bufferedReader.close();
                 message = stringBuilder.toString();
 
+            } catch (IOException e) {
+                Log.e("IOException", "no internet?");
+                Toast.makeText(getBaseContext(), "No internet connection.", Toast.LENGTH_SHORT).show();
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
