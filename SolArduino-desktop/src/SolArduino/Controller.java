d5c00b18b7c5c7754fbd3ee9e4cdf3b6e701d555
Thomas <hollandpirates@gmail.com>
2016-10-23 09:26:22 +0200

    added desktop app

---
+++ SolArduino-desktop/src/SolArduino/Controller.java
@@ -0,0 +1,73 @@
+package SolArduino;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+import static com.sun.corba.se.impl.util.Utility.printStackTrace;
+
+public class Controller {
+
+    String username = System.getProperty("user.name");
+
+    @FXML private Text text;
+    @FXML private TextField inputDegrees;
+    @FXML private ImageView buttonDownImage;
+
+    @FXML protected void buttonUp(MouseEvent event) {
+        if(event.getEventType().equals(MouseEvent.MOUSE_PRESSED)) {
+            sendHttpRequest("http://192.168.178.106/?panel=up");
+        } else { // mouse released
+            sendHttpRequest("http://192.168.178.106/?panel=stop");
+        }
+    }
+
+    @FXML protected void buttonDown(MouseEvent event) {
+        if(event.getEventType().equals(MouseEvent.MOUSE_PRESSED)) {
+            sendHttpRequest("http://192.168.178.106/?panel=down");
+        } else { // mouse released
+            sendHttpRequest("http://192.168.178.106/?panel=stop");
+        }
+    }
+
+    @FXML protected void buttonAuto(ActionEvent event) {
+        sendHttpRequest("http://192.168.178.106/?panel=auto");
+    }
+
+    @FXML protected void buttonUpdate(ActionEvent event) {
+        sendHttpRequest("http://192.168.178.106/?update");
+    }
+
+    @FXML protected void setAngle(ActionEvent event) {
+        sendHttpRequest("http://192.168.178.106/?degrees="+inputDegrees.getText());
+    }
+
+    private void sendHttpRequest(String url) {
+        System.out.println("sending request to"+url);
+//        try {
+//            InputStream response = new URL(url).openStream();
+//            try (Scanner scanner = new Scanner(response)) {
+//                String responseBody = scanner.useDelimiter("\\A").next();
+//                text.setText(responseBody);
+//            }
+//        } catch (IOException e) {
+//            printStackTrace();
+//        }
+
+    }
+}
