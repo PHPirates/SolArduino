81d1a9eaf9a696e547c40743a4d648c55edf1ccf
Thomas <hollandpirates@gmail.com>
2016-10-23 12:31:49 +0200

    fixed #18, http request on seperate thread

--- SolArduino-desktop/src/SolArduino/Controller.java
+++ SolArduino-desktop/src/SolArduino/Controller.java
@@ -25,49 +25,65 @@ public class Controller {
 
     String username = System.getProperty("user.name");
 
+    String ip = "http://192.168.178.106/?";
+
     @FXML private Text text;
     @FXML private TextField inputDegrees;
     @FXML private ImageView buttonDownImage;
 
     @FXML protected void buttonUp(MouseEvent event) {
         if(event.getEventType().equals(MouseEvent.MOUSE_PRESSED)) {
-            sendHttpRequest("http://192.168.178.106/?panel=up");
+            sendHttpRequest("panel=up");
         } else { // mouse released
-            sendHttpRequest("http://192.168.178.106/?panel=stop");
+            sendHttpRequest("panel=stop");
         }
     }
 
     @FXML protected void buttonDown(MouseEvent event) {
         if(event.getEventType().equals(MouseEvent.MOUSE_PRESSED)) {
-            sendHttpRequest("http://192.168.178.106/?panel=down");
+            sendHttpRequest("panel=down");
         } else { // mouse released
-            sendHttpRequest("http://192.168.178.106/?panel=stop");
+            sendHttpRequest("panel=stop");
         }
     }
 
     @FXML protected void buttonAuto(ActionEvent event) {
-        sendHttpRequest("http://192.168.178.106/?panel=auto");
+        sendHttpRequest("panel=auto");
     }
 
     @FXML protected void buttonUpdate(ActionEvent event) {
-        sendHttpRequest("http://192.168.178.106/?update");
+        sendHttpRequest("update");
     }
 
     @FXML protected void setAngle(ActionEvent event) {
-        sendHttpRequest("http://192.168.178.106/?degrees="+inputDegrees.getText());
+        if (inputDegrees.getText() != null) {
+            sendHttpRequest("degrees=" + inputDegrees.getText());
+        }
     }
 
-    private void sendHttpRequest(String url) {
-        System.out.println("sending request to"+url);
-//        try {
-//            InputStream response = new URL(url).openStream();
-//            try (Scanner scanner = new Scanner(response)) {
-//                String responseBody = scanner.useDelimiter("\\A").next();
-//                text.setText(responseBody);
-//            }
-//        } catch (IOException e) {
-//            printStackTrace();
-//        }
+    private void sendHttpRequest(String urlparam) {
+        final String url = ip+urlparam;
+
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                System.out.println("sending request to "+url);
+                try {
+                    InputStream response = new URL(url).openStream();
+                    try (Scanner scanner = new Scanner(response)) {
+                        String responseBody = scanner.useDelimiter("\\A").next();
+                        text.setText(responseBody);
+                    }
+                } catch (IOException e) {
+                    System.out.println("Request to "+url+" failed.");
+                    printStackTrace();
+                }
+                System.out.println("thread exiting");
+            }
+
+        });
+        thread.start();
+
 
     }
 }
