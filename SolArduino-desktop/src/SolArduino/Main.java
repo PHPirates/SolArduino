6d28a405a13acb1f071a7b5874b1ebeea7cd95cd
Thomas <hollandpirates@gmail.com>
2016-10-23 11:11:13 +0200

    re-added icon

--- SolArduino-desktop/src/SolArduino/Main.java
+++ SolArduino-desktop/src/SolArduino/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -17,6 +18,7 @@ public class Main extends Application {
         Scene scene = new Scene(root, 500, 300);
         File file = new File("src/SolArduino/resources/stylesheet.css");
         scene.getStylesheets().add(file.toURI().toString());
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("ic_solarduino.png")));
         primaryStage.setScene(scene);
         primaryStage.show();
     }
