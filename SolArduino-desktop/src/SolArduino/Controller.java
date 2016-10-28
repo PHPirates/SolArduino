e1f507df79eefd7e31fa044f7f66a01658cbe7b4
slideclimb <abbyberkers@gmail.com>
2016-10-28 17:21:31 +0200

    slider works

--- SolArduino-desktop/src/SolArduino/Controller.java
+++ SolArduino-desktop/src/SolArduino/Controller.java
@@ -1,15 +1,18 @@
 package SolArduino;
 
 import com.sun.org.apache.xpath.internal.SourceTree;
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
+import javax.annotation.Resources;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,13 +24,26 @@ import java.util.Scanner;
 
 import static com.sun.corba.se.impl.util.Utility.printStackTrace;
 
-public class Controller {
+public class Controller implements Initializable{
 
     String username = System.getProperty("user.name");
 
-    @FXML private Text text;
+    int angle = 42;
+
+    @FXML private Slider slider;
+    @FXML private Text sliderText;
+    @FXML private Button buttonSetAngle;
     @FXML private TextField inputDegrees;
     @FXML private ImageView buttonDownImage;
+
+    @FXML public void initialize(URL location, ResourceBundle resourceBundle){
+        slider.valueProperty().addListener((observable, oldValue, newValue)-> {
+            sliderText.setText(String.valueOf(newValue.intValue()));
+            angle = newValue.intValue();
+            buttonSetAngle.setText("set panels at " + angle + " degrees" );
+
+        });
+    }
 
     @FXML protected void buttonUp(MouseEvent event) {
         if(event.getEventType().equals(MouseEvent.MOUSE_PRESSED)) {
@@ -54,7 +70,7 @@ public class Controller {
     }
 
     @FXML protected void setAngle(ActionEvent event) {
-        sendHttpRequest("http://192.168.178.106/?degrees="+inputDegrees.getText());
+        sendHttpRequest("http://192.168.178.106/?degrees="+angle);
     }
 
     private void sendHttpRequest(String url) {
