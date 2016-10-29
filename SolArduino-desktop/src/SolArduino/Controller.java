950c041b8caca0eb6283f7be34c397040b87fd31
slideclimb <abbyberkers@gmail.com>
2016-10-29 16:17:38 +0200

    added graph to second tab

--- SolArduino-desktop/src/SolArduino/Controller.java
+++ SolArduino-desktop/src/SolArduino/Controller.java
@@ -5,6 +5,8 @@ import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
@@ -19,6 +21,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 
@@ -31,9 +34,14 @@ public class Controller implements Initializable{
     int angle = 42;
 
     @FXML private Slider slider;
-    @FXML private Text sliderText;
     @FXML private Button buttonSetAngle;
+    @FXML private LineChart graph;
     @FXML private TextField inputDegrees;
+
+    double[][] graphData = {
+            {0,0},{1,0},{2,0},{3,0},{4,0},{5,0},{6,100},{7,250},{8,500}, {9,750},{10,1000},{11,1200},{12,2000},
+            {13,3100},{14,3600},{15,3700},{16,3400},{17,2700},{18,1900},{19,900},{20,400},{21,0},{22,0},{23,0},{24,0}
+    };
 
     @FXML public void initialize(URL location, ResourceBundle resourceBundle){
         slider.valueProperty().addListener((observable, oldValue, newValue)-> {
@@ -46,6 +54,14 @@ public class Controller implements Initializable{
             }
 
         });
+
+        XYChart.Series series = new XYChart.Series();
+        series.setName("power");
+        for (double[] aGraphData : graphData) {
+            series.getData().add(new XYChart.Data<>(aGraphData[0], aGraphData[1]));
+
+        }
+        graph.getData().add(series);
     }
 
     @FXML protected void buttonUp(MouseEvent event) {
