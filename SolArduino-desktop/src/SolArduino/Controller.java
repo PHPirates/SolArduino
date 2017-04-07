032c142eb8f3e9b416ac94d22e0e5888a0e4b38d
Thomas <hollandpirates@gmail.com>
2017-04-07 14:30:48 +0200

    changed version number

--- SolArduino-desktop/src/SolArduino/Controller.java
+++ SolArduino-desktop/src/SolArduino/Controller.java
@@ -48,7 +48,7 @@ public class Controller implements Initializable {
     
     private int timeout = 1000; // timeout for sending up/down requests
     private String ip = "http://192.168.8.42/?"; // ip address from the Arduino
-    private String currentVersionString = "Version 1.2";
+    private String currentVersionString = "Version 1.2.1";
     // current version of the desktop app
     private String lastVersionString;
     private String checkVersionLink =
