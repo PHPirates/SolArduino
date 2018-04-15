6124a51ea74241721b6c1ae73818261c994a4158
Thomas <hollandpirates@gmail.com>
2018-04-15 13:11:33 +0200

    add potmeter unit test

---
+++ UnitTests/testPotMeter/testPotMeter.ino
@@ -0,0 +1,23 @@
+const byte POTMETERPIN = A7;
+
+const int SAMPLE_RATE = 1; //amount of readings to take the average of when reading the potmeter
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(POTMETERPIN,INPUT);
+
+}
+
+void loop() {
+  
+  Serial.println(readPotMeter());
+  delay(1000);
+}
+
+int readPotMeter() {
+  long total = 0;
+  for (int i=0; i<SAMPLE_RATE; i++) {
+    total += analogRead(POTMETERPIN);
+  }
+  return total/SAMPLE_RATE;
+}
