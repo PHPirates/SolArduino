544df1a6a561e277480466b1db335d6969a0accc
Thomas <hollandpirates@gmail.com>
2018-07-31 12:04:42 +0200

    Add sunposition function

--- SolArduino-core/haskell/solarduino-haskell/src/SunPosition.hs
+++ SolArduino-core/haskell/solarduino-haskell/src/SunPosition.hs
@@ -4,9 +4,26 @@ import Data.Astro.Time.JulianDate
 import Data.Astro.Coordinate
 import Data.Astro.Types
 import Data.Astro.Sun
+import Data.Astro.Effects
 
 ro :: GeographicCoordinates
 ro = GeoC (fromDMS 51 32 48) (-(fromDMS 4 24 42))
 
-dt :: LocalCivilTime
-dt = lctFromYMDHMS (DH 1) 2018 7 30 10 29 0
+--dt :: LocalCivilTime
+--dt = lctFromYMDHMS (DH 1) 2018 7 30 10 29 0
+
+--jd :: JulianDate
+--jd = lctUniversalTime dt
+
+--ec1 :: EquatorialCoordinates1
+--ec1 = sunPosition2 jd
+
+-- | Get the sun position from local time. Location is hard-coded.
+getSunPosition :: LocalCivilTime -- ^ Local time, for example with lctFromYMDHMS (DH 1) 2018 7 30 10 29 0
+            -> HorizonCoordinates -- ^ The altitude and azimuth, e.g. HC {hAltitude = DD 49.312050979507404, hAzimuth = DD 118.94723825710143}
+getSunPosition date = ec1ToHC ro jd ec1
+    where jd = lctUniversalTime date
+          ec1 = sunPosition2 jd
+
+--hc :: HorizonCoordinates
+--hc = ec1ToHC ro jd ec1
--- SolArduino-core/haskell/solarduino-haskell/test/CoreTest.hs
+++ SolArduino-core/haskell/solarduino-haskell/test/CoreTest.hs
@@ -2,19 +2,30 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 import PowerFunctions
+import SunPosition (getSunPosition)
+import Data.Astro.Time.JulianDate (lctFromYMDHMS)
+import Data.Astro.Types
+import Data.Astro.Coordinate
 
 -- to run tests in terminal: stack test
 main = defaultMain tests
 
-tests :: TestTree
-tests = testGroup "Tests" [unitTests]
-
 -- Output is compared with the original Mathematica implementation
-unitTests =
+tests :: TestTree
+tests = testGroup "Tests" [directPowerTest, sunPositionTest]
+
+directPowerTest =
     testGroup
-        "Unit Tests"
+        "directPower Tests"
         [ testCase "Test directPower 0.5 10" $ compare (abs (directPower 0.5 10 - 489.607)) 0.001 @?= LT
         , testCase "Test directPower 0.34 253" $ compare (abs (directPower 0.34 253 - 526.962)) 0.001 @?= LT
         , testCase "Test directPower 0 0" $ compare (abs (directPower 0 0 - 576.186)) 0.001 @?= LT
         , testCase "Test directPower 3.14 10" $ compare (abs (directPower 3.14 10)) 0.001 @?= LT
+        ]
+
+sunPositionTest =
+    testGroup
+        "sunPosition tests"
+        [ testCase "Test sunPosition" $
+          getSunPosition (lctFromYMDHMS (DH 1) 2018 7 30 10 29 0) @?= HC (DD 42.375915495531196) (DD 117.3390445162529)
         ]
