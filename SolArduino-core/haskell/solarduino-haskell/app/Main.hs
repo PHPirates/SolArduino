354d37d9502d91cbb54f28aa08608e6ef28007e2
Abby Berkers <abbyberkers@gmail.com>
2019-07-27 12:32:28 +0200

    typo

--- SolArduino-core/haskell/solarduino-haskell/app/Main.hs
+++ SolArduino-core/haskell/solarduino-haskell/app/Main.hs
@@ -13,7 +13,7 @@ import           TimeConverters
 --main = print (directPower 10 0.5)
 -- | Benchmark functions using Criterion.
 main = defaultMain [
-    bgroup "bechmarking..." [ -- would usually be function name here
+    bgroup "benchmarking..." [ -- would usually be function name here
     -- whnf wants two arguments: a function of one argument and the argument
 --        bench "2018 7 30 10 29 0" $ whnf (toLocalTime 2018 7 30 10 29) 0
 --        bench "2018 8 11 22 0 0" $ whnf (bestAngle (toUniversalTime 2018 8 11 3 0 0) (toUniversalTime 2018 8 11 22 0 0)) 100 -- 5 ms
