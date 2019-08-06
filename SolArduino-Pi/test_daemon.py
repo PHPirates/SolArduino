a1a4cd2573a7bd7474025e86745a7ceaf96623fc
Thomas <hollandpirates@gmail.com>
2019-08-06 12:04:47 +0200

    Kill old process before starting new one

--- SolArduino-Pi/test_daemon.py
+++ SolArduino-Pi/test_daemon.py
@@ -1,21 +1,28 @@
+import time
+
+import psutil as psutil
 from pep3143daemon import DaemonContext, PidFile
-import syslog, time
 
 counter = 0
-# todo kill previous process before starting another one
-pid = '/tmp/pep3143daemon_example.pid'
+pid_path = '/tmp/pep3143daemon_example.pid'
 
-pidfile = PidFile(pid)
+# Kill old process before starting new one
+try:
+    with open(pid_path, 'r') as f:
+        pid = int(f.read())
+
+    process = psutil.Process(pid)
+    process.terminate()
+except FileNotFoundError:
+    pass
+
+pidfile = PidFile(pid_path)
 daemon = DaemonContext(pidfile=pidfile)
-# we could have written this also as:
-# daemon.pidfile = pidfile
 
-print('pidfile is: {0}'.format(pid))
+print('pidfile is: {0}'.format(pid_path))
 print('daemonizing...')
 
 daemon.open()
-
-# todo create shutdown method by overriding terminate?
 
 with open("/tmp/current_time.txt", "w") as f:
     f.write('pep3143daemon_example: daemonized')
@@ -23,7 +30,7 @@ with open("/tmp/current_time.txt", "w") as f:
 while True:
     counter += 1
     with open("/tmp/current_time.txt", "w") as f:
-        f.write("The time is now " + time.ctime())
+        f.write("The time is now " + time.ctime() + '\n')
         f.write('pep3143daemon_example: counter: {0}'.format(counter))
     time.sleep(2)
 
